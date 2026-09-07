04c13faa83414c8d4ac5370ec7bffdf7dd467f60
JVN Studio Network <jvnstudio.net@gmail.com>
2026-09-07 19:13:21 -0400

    Add Infrastructure Manager workload outputs

---
+++ infra-manager/outputs.tf
@@ -0,0 +1,29 @@
+output "vpc_name" {
+  value       = google_compute_network.medicare.name
+  description = "Infrastructure Manager demo VPC."
+}
+
+output "primary_mig" {
+  value       = google_compute_region_instance_group_manager.primary.name
+  description = "Primary regional managed instance group."
+}
+
+output "gke_cluster_name" {
+  value       = var.enable_gke ? google_container_cluster.primary[0].name : null
+  description = "Regional GKE cluster name."
+}
+
+output "object_bucket" {
+  value       = google_storage_bucket.portal_objects.name
+  description = "Versioned Cloud Storage bucket."
+}
+
+output "dr_mig" {
+  value       = var.enable_dr ? google_compute_region_instance_group_manager.dr[0].name : null
+  description = "Optional warm DR regional managed instance group."
+}
+
+output "filestore_ip" {
+  value       = var.enable_filestore ? google_filestore_instance.shared[0].networks[0].ip_addresses[0] : null
+  description = "Optional Filestore IP address."
+}
