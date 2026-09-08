c4f6fb3340cc1dd69147093d5c490ed2349ac0ff
JVN Studio Network <jvnstudio.net@gmail.com>
2026-09-08 16:48:33 -0400

    Add DR autoscaling and load balancer inputs

--- infra-manager/single-project/variables.tf
+++ infra-manager/single-project/variables.tf
@@ -10,7 +10,7 @@ variable "primary_region" {
 }
 
 variable "dr_region" {
-  description = "Secondary region used for DR network and optional warm capacity."
+  description = "Secondary region used for warm DR capacity."
   type        = string
   default     = "us-central1"
 }
@@ -22,15 +22,33 @@ variable "vm_target_size" {
 }
 
 variable "enable_gke" {
-  description = "Deploy a regional GKE cluster and node pool. Disabled for the first quota-safe deployment."
+  description = "Deploy a regional GKE cluster and node pool."
   type        = bool
   default     = false
 }
 
 variable "enable_dr" {
-  description = "Deploy warm VM capacity in the DR region."
+  description = "Deploy warm VM capacity in the DR region and attach it to the global load balancer."
   type        = bool
-  default     = false
+  default     = true
+}
+
+variable "dr_max_replicas" {
+  description = "Maximum number of DR VMs that can be started automatically by the DR autoscaler."
+  type        = number
+  default     = 3
+}
+
+variable "lb_max_rate_per_instance" {
+  description = "Backend serving capacity in requests per second per VM. Kept low so autoscaling is easy to demonstrate."
+  type        = number
+  default     = 5
+}
+
+variable "dr_lb_target_utilization" {
+  description = "Fraction of load-balancer serving capacity the DR autoscaler tries to maintain."
+  type        = number
+  default     = 0.6
 }
 
 variable "enable_filestore" {
